bf3f26fd0c2e291c68052a23c77e6bb5820a1600
Andrew Kelley <andrew@ziglang.org>
2022-11-11 17:22:27 -0700

    build: link libc
    this is closer to what it will be like when the code is written in C and
    uses libc memcpy/memset.

--- build.zig
+++ build.zig
@@ -12,6 +12,7 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("zig-wasi", "src/main.zig");
+    exe.linkLibC();
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
